d61e82f10d19f9e304e8e616057dc0a333ebcd04
B. M. Mahbubul Alam <108087360+phmahbub@users.noreply.github.com>
2022-10-06 13:11:17 +0600

    A complete website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, createBrowserRouter, RouterProvider } from 'react-router-dom'
+import About from './components/About'
+import Book from './components/Book'
+import BookDetails from './components/BookDetails'
+import Books from './components/Books'
+import Home from './components/Home'
+import Main from './components/Main'
+import NewFile from './components/NewFile'
 
 function App() {
+  const router = createBrowserRouter([
+    {
+    path:'/', 
+    element:<Main></Main>,
+    children:[
+      {path:'/home', element:<Home/>},
+      {path:'/', element:<Home/>},
+      {path:'/books', element:<Books/>,
+    loader:()=>{
+      return fetch('https://api.itbook.store/1.0/new')
+    }},
+      {path:'/book/:id', element:<BookDetails/>,
+    loader:({params})=>{
+      return fetch(`https://api.itbook.store/1.0/books/${params.id}`)
+    }},
+      {path:'/about', element:<About/>},
+      {path:'/newfile', element:<NewFile></NewFile>},
+    ]
+    },
+  ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <RouterProvider router={router}></RouterProvider>
+  )
 }
 
-export default App;
+export default App
